dedb3da04f677cb7c9cad19f743200df6f5e13b5
Richard Urunuela <richardurunuela@MacBook-Air-de-Richard.local>
2017-10-13 15:10:46 +0200

    create

---
+++ src/com/company/Hamming.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by richardurunuela on 13/10/2017.
+ */
+public class Hamming {
+    private int hammingDistance;
+
+    public Hamming(String s, String s1) {
+    }
+
+    public int getHammingDistance() {
+        return hammingDistance;
+    }
+}
